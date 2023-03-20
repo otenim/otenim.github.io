5ece0434bd53222d535b5597e725e85003eb0323
otenim <snake_istobelieve@keio.jp>
2023-03-20 20:30:32 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 // Step 1: Import React
 import * as React from 'react'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by followung the Gatsby Tutorial.</p>
+    </Layout>
   )
 }
 
