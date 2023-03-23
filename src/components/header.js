8efa556426f7ca906f491fb322b8a552dbe6a01e
otenim <snake_istobelieve@keio.jp>
2023-03-23 10:55:46 +0900

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,26 @@
 import * as React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import { Link } from 'gatsby'
 
 
 const Header = () => {
   return (
     <header className="bg-dark">
-      <Navbar expand="md" variant="dark">
-        <Navbar.Brand href="/">Otenim's Personal Web Page</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarResponsive" />
-        <Navbar.Collapse id="navbarResponsive">
-          <Nav as="ul" className="ml-auto">
-            <Nav.Item as="li">
-              <Link to="/pubs" className="nav-link" activeClassName="active">Publifications</Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Container>
+        <Navbar expand="md" variant="dark">
+          <Navbar.Brand href="/">Otenim's Personal Web Page</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarResponsive" />
+          <Navbar.Collapse id="navbarResponsive">
+            <Nav as="ul" className="ml-auto">
+              {/* <Nav.Item as="li">
+                <Link to="/pubs" className="nav-link" activeClassName="active">Publifications</Link>
+              </Nav.Item> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
     </header>
   )
 }
-
 
 export default Header
